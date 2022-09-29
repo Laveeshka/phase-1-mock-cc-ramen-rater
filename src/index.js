3a924a03645226e5a0d4621531fc53f4076e18cf
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-29 21:05:39 +1000

    completed extra advanced deliverables

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@
 const baseUrl =  "http://localhost:3000/ramens"
 const ramenMenu = document.getElementById("ramen-menu");
 const ramenDetail = document.getElementById("ramen-detail");
+let img = ramenDetail.querySelector(".detail-image");
+let h2 = ramenDetail.querySelector(".name");
+let h3 = ramenDetail.querySelector(".restaurant");
 let span = document.getElementById("rating-display");
 let p = document.getElementById("comment-display");
 const ramenForm = document.getElementById("new-ramen");
+const editRamenForm = document.getElementById("edit-ramen");
+let deleteBtn = document.querySelector(".delete-ramen");
 
 //holds all functions
 function init(){
     getAllRamen();
+    showFirstRamenDetails();
     eventListeners();
 }
 
@@ -37,24 +43,26 @@ function renderOneRamen(ramen){
 function eventListeners(){
     ramenMenu.addEventListener("click", clickRamenImageHandler);
     ramenForm.addEventListener("submit", submitNewRamenHandler);
+    editRamenForm.addEventListener("submit", editFeaturedRamenHandler);
+    deleteBtn.addEventListener("click", deleteRamenHandler);
 }
 
 function clickRamenImageHandler(event){
     //console.log(event.target.id);
-    let img = ramenDetail.querySelector(".detail-image");
-    let h2 = ramenDetail.querySelector(".name");
-    let h3 = ramenDetail.querySelector(".restaurant");
+
 
     //GET the ramen by id
     fetch(`${baseUrl}/${event.target.id}`)
         .then(res => res.json())
         .then(ramenObj => {
+            img.setAttribute("id", `img-${ramenObj.id}`);
             img.src = ramenObj.image;
             img.alt = ramenObj.name;
             h2.textContent = ramenObj.name;
             h3.textContent = ramenObj.restaurant;
             span.textContent = ramenObj.rating;
             p.textContent = ramenObj.comment;
+            deleteBtn.setAttribute("id", `btn-${ramenObj.id}`);
         })
 }
 
@@ -90,6 +98,67 @@ function addNewRamentoDatabase(newRamenObj){
     })
         .then(res => res.json())
         .then(data => renderOneRamen(data))
+}
+
+function showFirstRamenDetails(){
+     //GET the first ramen (id: 1)
+     fetch(`${baseUrl}/1`)
+     .then(res => res.json())
+     .then(ramenObj => {
+        img.setAttribute("id", `img-${ramenObj.id}`);
+         img.src = ramenObj.image;
+         img.alt = ramenObj.name;
+         h2.textContent = ramenObj.name;
+         h3.textContent = ramenObj.restaurant;
+         span.textContent = ramenObj.rating;
+         p.textContent = ramenObj.comment;
+         deleteBtn.setAttribute("id", `btn-${ramenObj.id}`);
+     })
+}
+
+function editFeaturedRamenHandler(event){
+    event.preventDefault();
+
+    const ramenId = img.id.charAt(img.id.length -1);
+    console.log(ramenId);
+    console.log(event.target.rating.value);
+    console.log(event.target["new-comment"].value)
+
+    fetch(`${baseUrl}/${ramenId}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            rating: parseInt(event.target.rating.value),
+            comment: event.target["new-comment"].value
+        })
+    })
+        .then(res => res.json())
+        .then(ramenObj => {
+            span.textContent = ramenObj.rating;
+            p.textContent = ramenObj.comment;
+        });
+
+    editRamenForm.reset();
+}
+
+function deleteRamenHandler(event){
+    const ramenId = deleteBtn.id.charAt(img.id.length -1);
+
+    fetch(`${baseUrl}/${ramenId}`, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(res => res.json())
+        .then(ramenObj => {
+            console.log(ramenObj);
+            ramenMenu.innerHTML = '';
+            init();
+        })
 }
 
 init();
